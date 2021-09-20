e981a906c1270839731a0eb6783abbca19b61865
Olivier Girardot <olive_girardot@hotmail.com>
2021-09-20 15:59:04 +0200

    uncommented some stuff for clarity

--- src/Button.js
+++ src/Button.js
@@ -1,35 +1,17 @@
 import React, { Component } from "react";
 
-// class Button extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { clicked: false };
-//     this.handleClick = this.handleClick.bind(this);
-//     // bind the function to this instance
-//   }
-
-//   handleClick(e) {
-//     this.setState({ clicked: true });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.state.clicked ? 'clicked' : 'not clicked'}</h1>
-//         <button onClick={this.handleClick}>Click Me</button>
-//       </div>
-//     )
-//   }
-// }
-
-// alternate syntax using Babel
-
 class Button extends Component {
-  state = { clicked: false };
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+    this.handleClick = this.handleClick.bind(this);
+    // bind the function to this instance
+  }
 
-  handleClick = e => {
+  handleClick(e) {
     this.setState({ clicked: true });
   }
+
   render() {
     return (
       <div>
@@ -39,5 +21,23 @@ class Button extends Component {
     )
   }
 }
+
+// alternate syntax using Babel
+
+// class Button extends Component {
+//   state = { clicked: false };
+
+//   handleClick = e => {
+//     this.setState({ clicked: true });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.clicked ? 'clicked' : 'not clicked'}</h1>
+//         <button onClick={this.handleClick}>Click Me</button>
+//       </div>
+//     )
+//   }
+// }
 
 export default Button;
